183f5193173e88856acbf628dbe72f3b2705fb73
david <david@davids-MacBook-Pro.local>
2021-12-17 16:26:15 -0500

    f

---
+++ _build/jupyter_execute/Module1/Programming_Assignment_1.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Week 1 Programming Assignment 
+# 
+# Remark: 
+# 
+# (1) There are 9 problems, among which 7 problems are for 10 points whlie the other 2 problems are optional. Although the optional problems will not be counted, you are suggested to finish them as possible as you can.
+# 
+# (2) Please upload your solutions of this assignment to Canvas with a file named "Programming_Assignment_1 _yourname.ipynb" before 11:59pm May 23.
+
+# =================================================================================================================
+
+# ### **Problem 1 (1 pt).** Plot the curve of function 
+# ### $$f(x) = x^3 - x,~~ x \in[-1,1].$$
+
+# In[1]:
+
+
+# write your code for solving probelm 1 in this cell
+
+
+# =================================================================================================================
+
+# ### **Problem 2 (2 pts).** 
+# ### (1) Find the two roots of the function
+# ### $$f(x) = 2x^2 - x - 1,~~ x \in[-1,2].$$
+# 
+# ### (2) Plot the curve of the function $f(x)$ defined in (1) and mark all the roots on the curve.
+
+# =================================================================================================================
+
+# ### **Problem 3 (optional)**.  Given $P=\begin{bmatrix} 1 & 2 \\ 3 & 4\end{bmatrix}$, investigate the two different multiplications $P*P$ and $torch.mm(P,P)$.
+
+# =================================================================================================================
+
+# ### **Problem 4 (1 pt).**  Given 
+# ### $$A=\begin{bmatrix} 1 & 2 & 3 \\ 4 & 5 & 6 \\ 7 & 8 & 9 \end{bmatrix}, ~~ b=\begin{bmatrix} 1  \\ 2 \\ 3\end{bmatrix}.$$
+# ### Solve $A^2 x = b.$
+
+# =================================================================================================================
+
+# ### **Problem 5 (1 pt).**  Given $f(x,y) = x^2+y^2+(xy)^3$, compute $\frac{\partial f(x,y)}{\partial y}$ at $x=1,y=2$.
+
+# =================================================================================================================
+
+# ### **Problem 6 (optional).** Define a function to find the minimum of three numbers $a,~b,~c$. Test your code and print the minimum of the three numbers, where $a=\sqrt{2},~ b=\frac{4}{3},~ c=0.5e$.
+
+# =================================================================================================================
+
+# 
+# ### **Problem 7 (1 pt).** Define a function to find the maximum and minimum of a sequence with n numbers.
+# 
+# Hint: x = np.random.randint(a,b,size=n) can randomly generate n numbers (saved in a row vector x ) and each number is between a and b.
+# 
+
+# =================================================================================================================
+
+# ### **Problem 8 (1 pt).**  Define a function to sort a sequence with n numbers in ascending order.
+# 
+
+# =================================================================================================================
+
+# ### **Problem 9 (3 pts).**  Given a function 
+# ### $$f(x,y)=(x-2)^2 + y^2 + 2(y-4)^2 + 2x^2 + 2xy + 4y -16x +1.$$
+# ### Please write a code to apply gradient descent method to find the minimum of $f(x,y)$ with initial value $x=y=0$.
+# 
--- _build/jupyter_execute/Module1/m1_02.py
+++ _build/jupyter_execute/Module1/m1_02.py
@@ -17,6 +17,7 @@ IFrame(src="https://cdnapisec.kaltura.com/p/2356971/sp/235697100/embedIframeJs/u
 IFrame(src="https://cdnapisec.kaltura.com/p/2356971/sp/235697100/embedIframeJs/uiconf_id/41416911/partner_id/2356971?iframeembed=true&playerId=kaltura_player&entry_id=1_awemnq71&flashvars[streamerType]=auto&amp;flashvars[localizationCode]=en&amp;flashvars[leadWithHTML5]=true&amp;flashvars[sideBarContainer.plugin]=true&amp;flashvars[sideBarContainer.position]=left&amp;flashvars[sideBarContainer.clickToClose]=true&amp;flashvars[chapters.plugin]=true&amp;flashvars[chapters.layout]=vertical&amp;flashvars[chapters.thumbnailRotator]=false&amp;flashvars[streamSelector.plugin]=true&amp;flashvars[EmbedPlayer.SpinnerTarget]=videoHolder&amp;flashvars[dualScreen.plugin]=true&amp;flashvars[hotspots.plugin]=1&amp;flashvars[Kaltura.addCrossoriginToIframe]=true&amp;&wid=1_tiw3d9xz"  ,width='800', height='500')
 
 
+# ## Definition of linearly separable sets
 # In this section, we consider a special class of $k$ linearly separable
 # sets for $k\ge 2$. Let us first introduce the following definition for
 # binary classification.
