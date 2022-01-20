48e3c0118369e52a9b825ac3a0f939da0fc71498
david <david@davids-MacBook-Pro.local>
2022-01-20 02:06:54 -0500

    m

--- _build/jupyter_execute/Module0/quiz0.py
+++ _build/jupyter_execute/Module0/quiz0.py
@@ -11,6 +11,7 @@ import ipywidgets as widgets
 
 # ## Queation 1
 # ![image](./Quiz1_Question1.jpg)
+# 
 # Consider the feasible region given by the following inequalities. whose boundary lines are graphed above.
 # 
 # $$
@@ -26,13 +27,85 @@ import ipywidgets as widgets
 # In[2]:
 
 
-widgets.RadioButtons(
+def on_change(change):
+    if change['type'] == 'change' and change['name'] == 'value':
+        if change['new'] == 'I':
+            print( "Your answer: %s     is correct"% change['new'] )
+        else:
+            print( "Your answer: %s     is wrong"% change['new'] )
+
+w0 = widgets.RadioButtons(
     options=['I', 'II', 'III','IV','None'],
 #    value='pineapple', # Defaults to 'pineapple'
 #    layout={'width': 'max-content'}, # If the items' names are long
     description='',
     disabled=False
 )
+w0.observe(on_change)
+
+
+# In[3]:
+
+
+w0
+
+
+# In[4]:
+
+
+import numpy as np
+import nbinteract as nbi
+
+def normal(mean, sd):
+    '''Returns 1000 points drawn at random fron N(mean, sd)'''
+    return np.random.normal(mean, sd, 1000)
+
+# Pass in the `normal` function and let user change mean and sd.
+# Whenever the user interacts with the sliders, the `normal` function
+# is called and the returned data are plotted.
+nbi.hist(normal, mean=(0, 10), sd=(0, 2.0))
+
+
+# ```{dropdown} Show answer
+# Answer: III
+# ```
+
+# In[5]:
+
+
+w = widgets.Dropdown(
+    options=['Addition', 'Multiplication', 'Subtraction', 'Division'],
+    value='Addition',
+    description='Task:',
+)
+
+def on_change(change):
+    if change['type'] == 'change' and change['name'] == 'value':
+        print("changed to %s" % change['new'])
+
+w.observe(on_change)
+
+display(w)
+
+
+# In[6]:
+
+
+w
+
+
+# In[7]:
+
+
+button = widgets.Button(
+    description='Click me',
+    disabled=False,
+    button_style='', # 'success', 'info', 'warning', 'danger' or ''
+    tooltip='Click me',
+    icon='check' # (FontAwesome names without the `fa-` prefix)
+)
+button.on_click(on_change)
+button
 
 
 # ## Question 2
@@ -44,7 +117,7 @@ widgets.RadioButtons(
 # 
 # where $t$ is a scalar variable. Find $g^{'}(0)$ at $x=(1,1,1)$
 
-# In[3]:
+# In[8]:
 
 
 widgets.Dropdown(
@@ -63,7 +136,7 @@ widgets.Dropdown(
 # $$
 #  on the interval $[-2,3]$
 
-# In[4]:
+# In[9]:
 
 
 widgets.IntText(
@@ -73,7 +146,7 @@ widgets.IntText(
 )
 
 
-# In[5]:
+# In[10]:
 
 
 widgets.IntText(
