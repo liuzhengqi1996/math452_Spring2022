9ff9a7d5f5639f0bf78887df0ed1cb07a2970cd3
david <david@davids-MacBook-Pro.local>
2022-03-25 13:18:30 -0400

    m

--- _build/jupyter_execute/Module6/m6_01.py
+++ _build/jupyter_execute/Module6/m6_01.py
@@ -11,6 +11,8 @@ from IPython.display import IFrame
 IFrame(src="https://cdnapisec.kaltura.com/p/2356971/sp/235697100/embedIframeJs/uiconf_id/41416911/partner_id/2356971?iframeembed=true&playerId=kaltura_player&entry_id=1_luk1i2xr&flashvars[streamerType]=auto&amp;flashvars[localizationCode]=en&amp;flashvars[leadWithHTML5]=true&amp;flashvars[sideBarContainer.plugin]=true&amp;flashvars[sideBarContainer.position]=left&amp;flashvars[sideBarContainer.clickToClose]=true&amp;flashvars[chapters.plugin]=true&amp;flashvars[chapters.layout]=vertical&amp;flashvars[chapters.thumbnailRotator]=false&amp;flashvars[streamSelector.plugin]=true&amp;flashvars[EmbedPlayer.SpinnerTarget]=videoHolder&amp;flashvars[dualScreen.plugin]=true&amp;flashvars[hotspots.plugin]=1&amp;flashvars[Kaltura.addCrossoriginToIframe]=true&amp;&wid=1_s9zzktyl",width='800', height='500')
 
 
+# ## Download the lecture notes here: [Notes](https://sites.psu.edu/math452/files/2022/03/F01MgNet.pdf)
+
 # 1D and 2D Finite Element and Multigrid
 # -------------------------------------
 # 
@@ -24,27 +26,49 @@ IFrame(src="https://cdnapisec.kaltura.com/p/2356971/sp/235697100/embedIframeJs/u
 # 
 # ### Multigrid algorithm for $A * \mu=f$
 # 
-# Algorithm 11 A multigrid algorithm $\mu=\operatorname{MG} 1\left(f ; \mu^{0} ; J, v_{1}, \cdots, v_{J}\right)$ Set up
+# ```{prf:algorithm} A multigrid algorithm $\mu=\operatorname{MG} 1\left(f ; \mu^{0} ; J, v_{1}, \cdots, v_{J}\right)$
+# Set up:
 # 
+# $$
+#     f^{1}=f, \quad \mu^{1}=\mu^{0}
+# $$
 # 
-# $$f^{1}=f, \quad \mu^{1}=\mu^{0}$$ Smoothing and restriction from fine
-# to coarse level (nested)
+# Smoothing and restriction from fine to coarse level (nested)
 # 
-# for $\ell=1: J$ do
+# **For** $\ell=1: J$ do
 # 
-# for $i=1: v_{\ell}$ do
-# $$\mu^{\ell} \leftarrow \mu^{\ell}+S^{\ell} *\left(f^{\ell}-A_{\ell} * \mu^{\ell}\right)$$
-# end for
+# **For** $i=1: v_{\ell}$ do
+# 
+# $$
+#     \mu^{\ell} \leftarrow \mu^{\ell}+S^{\ell} *\left(f^{\ell}-A_{\ell} * \mu^{\ell}\right)
+# $$
+# 
+# **EndFor**
 # 
 # Form restricted residual and set initial guess:
-# $$\mu^{\ell+1} \leftarrow \Pi_{\ell}^{\ell+1} \mu^{\ell}, \quad f^{\ell+1} \leftarrow R *_{2}\left(f^{\ell}-A_{\ell} * \mu^{\ell}\right)+A_{\ell+1} * \mu^{\ell+1},$$
-# end for
+# 
+# $$
+#     \mu^{\ell+1} \leftarrow \Pi_{\ell}^{\ell+1} \mu^{\ell}, \quad f^{\ell+1} \leftarrow R *_{2}\left(f^{\ell}-A_{\ell} * \mu^{\ell}\right)+A_{\ell+1} * \mu^{\ell+1}
+# $$
+# 
+# **EndFor**
 # 
 # Prolongation and restriction from coarse to fine level
 # 
-# for $\ell=J-1: 1$ do
-# $$\mu^{\ell} \leftarrow \mu^{\ell}+R *_{2}^{\top}\left(\mu^{\ell+1}-\Pi_{\ell}^{\ell+1} \mu^{\ell}\right)$$
-# end for $$\mu \leftarrow \mu^{1}$$ 
+# **For** $\ell=J-1: 1$ do
+# 
+# $$
+#     \mu^{\ell} \leftarrow \mu^{\ell}+R *_{2}^{\top}\left(\mu^{\ell+1}-\Pi_{\ell}^{\ell+1} \mu^{\ell}\right)
+# $$
+# 
+# **EndFor**
+# 
+# $$
+#     \mu \leftarrow \mu^{1}
+# $$ 
+# 
+# ```
+# 
 # 
 # ```{admonition} Remark
 # The above multigrid method
