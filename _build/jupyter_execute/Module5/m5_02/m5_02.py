234f95a786fbf18a974b5d5f4802ed93765db3f9
david <david@davids-MacBook-Pro.local>
2022-03-25 13:34:07 -0400

    m

--- _build/jupyter_execute/Module5/m5_02/m5_02.py
+++ _build/jupyter_execute/Module5/m5_02/m5_02.py
@@ -11,7 +11,7 @@ from IPython.display import IFrame
 IFrame(src="https://cdnapisec.kaltura.com/p/2356971/sp/235697100/embedIframeJs/uiconf_id/41416911/partner_id/2356971?iframeembed=true&playerId=kaltura_player&entry_id=1_c20ifjjk&flashvars[streamerType]=auto&amp;flashvars[localizationCode]=en&amp;flashvars[leadWithHTML5]=true&amp;flashvars[sideBarContainer.plugin]=true&amp;flashvars[sideBarContainer.position]=left&amp;flashvars[sideBarContainer.clickToClose]=true&amp;flashvars[chapters.plugin]=true&amp;flashvars[chapters.layout]=vertical&amp;flashvars[chapters.thumbnailRotator]=false&amp;flashvars[streamSelector.plugin]=true&amp;flashvars[EmbedPlayer.SpinnerTarget]=videoHolder&amp;flashvars[dualScreen.plugin]=true&amp;flashvars[hotspots.plugin]=1&amp;flashvars[Kaltura.addCrossoriginToIframe]=true&amp;&wid=1_t1lki6t4"  ,width='800', height='500')
 
 
-# ## Download the lecture notes here: [Notes](https://sites.psu.edu/math452/files/2022/03/E01-02InitializationNormalization.pdf)
+# ## Download the lecture notes here: [Notes](https://sites.psu.edu/math452/files/2022/03/DetailedE01-02.pdf)
 
 # ## Recall the original DNN model
 # 
